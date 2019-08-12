b68076d6970eafdf134dbb28752b5e1abdd95fe0
J. Camilo G. Angarita <52009346+jcamiloangarita@users.noreply.github.com>
2019-08-12 09:07:17 +0200

    Update InitCond.py
    Added Description

--- InitCond.py
+++ InitCond.py
@@ -1,26 +1,26 @@
 def init_cond(Ts,riser,wtg,gt,zstep,tvd,deltaz):
     # Initial Conditions
-    Tdsio = [Ts]
-    Tdso = [Ts]
-    Tao = [Ts]
-    Tcsgo = [Ts]
-    Tsro = [Ts]
-    Tfm = [Ts]
+    Tdsio = [Ts]   # Temperature of the fluid inside the drill string at RKB
+    Tdso = [Ts]    # Temperature of the drill string wall at RKB, t=0
+    Tao = [Ts]      # Temperature of the fluid inside the annulus at RKB, t=0
+    Tcsgo = [Ts]    # Temperature of the casing at RKB, t=0   
+    Tsro = [Ts]    # Temperature of the surrounding space at RKB, t=0
+    Tfm = [Ts]      # Temperature of the formation at RKB
 
     for j in range(1, zstep):
 
         if j <= riser:
-            Tg = wtg
+            Tg = wtg    # Water Thermal Gradient for the Riser section
         else:
-            Tg = gt
+            Tg = gt      # Geothermal Gradient below the Riser section
 
         deltaT = Tsro[j - 1] + Tg*(tvd[j]-tvd[j-1])/deltaz
-
+        # Generating the Temperature Profile at t=0
         Tdsio.append(deltaT)
         Tdso.append(deltaT)
         Tao.append(deltaT)
         Tcsgo.append(deltaT)
         Tsro.append(deltaT)
-        if j < zstep:
-            Tfm.append(deltaT)
+        #if j < zstep:
+        Tfm.append(deltaT)
     return Tdsio, Tdso, Tao, Tcsgo, Tsro, Tfm
