055997e96e050478212e48b6e7cfb740ac7e3504
JUANG <camiloang94@gmail.com>
2019-08-19 17:27:51 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'pwptemp',
+  packages = ['pwptemp'], # this must be the same as the name above
+  version = '0.1',
+  description = 'my description',
+  author = 'jcamiloangarita',
+  author_email = '',
+  url = 'https://github.com/pro-well-plan/pwptemp', # use the URL to the github repo
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
