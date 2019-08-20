66ae8e8faa942c38fec176bff765cd1468135f1e
JUANG <camiloang94@gmail.com>
2019-08-20 08:46:29 +0200

    Packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'pwptemp',
-  packages = ['pwptemp'], # this must be the same as the name above
-  version = '0.1',
-  description = 'my description',
-  author = 'jcamiloangarita',
-  author_email = '',
-  url = 'https://github.com/pro-well-plan/pwptemp', # use the URL to the github repo
-  keywords = ['testing', 'logging', 'example'],
-  classifiers = [],
+    name='pwptemp',
+    packages=['pwptemp'],
+    version='0.1',
+    license='LGPL v3',
+    description='Well Temperature Distribution',
+    author='jcamiloangarita',
+    author_email='camiloangarita94@gmail.com',
+    url='https://github.com/pro-well-plan/pwptemp',
+    download_url='https://github.com/pro-well-plan/pwptemp/archive/v0.1.tr.gz',
+    keywords='well temperature distribution',
+    classifiers=['Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
 )
