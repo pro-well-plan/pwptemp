326e7255b89ff30aef7a3ed5ca51a083e87525d4
JUANG <camiloang94@gmail.com>
2019-08-22 09:30:49 +0200

    Packaging

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     download_url='https://github.com/pro-well-plan/pwptemp/archive/v0.0.2.tar.gz',
     keywords='well temperature distribution',
     classifiers=['Programming Language :: Python :: 3',
-                 'License :: OSI Approved :: GNU LGPL v3',
+                 'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                  'Natural Language :: English',
                  'Topic :: Scientific/Engineering',
                  'Topic :: Software Development',
