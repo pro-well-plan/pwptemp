538b9997357c5b9d97cfc57b2e65a142cfb0dad8
JUANG <camiloang94@gmail.com>
2019-09-01 20:39:59 +0200

    Adding Testing Module

--- pwptemp/tests/test_temp_calc.py
+++ pwptemp/tests/test_temp_calc.py
@@ -3,11 +3,13 @@ from pwptemp.InitCond import init_cond
 from pwptemp.WellPath import wellpath
 from pwptemp.LinearSystem import temp_calc
 
-class TestWellPath(TestCase):
-    def test_wellpath(self):
-        md, tvd, deltaz, zstep = wellpath(5000)
+
+class TestLinearSystem(TestCase):
+    def test_temp_calc(self):
+        tvd = wellpath(5000)[1]
         a, b, c, d, e, f = init_cond(15, 2, -0.005, 0.0238, 100, tvd, 50)
-        Tdsi, Ta, Tr, Tcsg, Tsr = temp_calc(15, a, b, c, d, e, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        tdsi, ta, tr, tcsg, tsr = temp_calc(15, a, b, c, d, e, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 100, 0, 500, 1000, 1500)
-        self.assertEqual(len(Tdsi), len(Ta))
+        self.assertEqual(len(tdsi), len(ta), len(tr))
+        self.assertEqual(len(tr), len(tcsg), len(tsr))
