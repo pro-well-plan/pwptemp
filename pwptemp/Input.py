149700dd8bdd424cac66b44d1a787729b656894a
JUANG <camiloang94@gmail.com>
2019-09-15 22:51:24 +0200

    Changes to be able to use any value for deltaz

--- pwptemp/Input.py
+++ pwptemp/Input.py
@@ -1,71 +1,25 @@
 from math import pi
 
-temp_dict = {
-  "tin": 20,
-  "ts": 15,
-  "wd": 0,
-  "csgco": 0,
-  "csgso": 0,
-  "csgio": 0,
-  "ddi": 0.101,
-  "ddo": 0.114,
-  "dcsg": 0.216,
-  "dcsgo":  0.24,
-  "dsr": 0.26,
-  "dsro": 0.6,
-  "dri": 0.45,
-  "dro": 0.5334,
-  "dfm": 2,
-  "dcsgci": 0.63,
-  "dcsgco": 0.66,
-  "dcsgsi": 0.41,
-  "dcsgso": 0.44,
-  "tcsr3": 0,
-  "tcem4": 0,
-  "q": 47.696,
-  "lambdal": 0.635,
-  "lambdac": 43.3,
-  "lambdacem": 0.7,
-  "lambdad": 40,
-  "lambdafm": 2.249,
-  "lambdar": 15.49,
-  "lambdarw": 5,
-  "lambdaw": 0.6,
-  "cl": 3713,
-  "cc": 469,
-  "ccem": 2000,
-  "cd": 400,
-  "cr": 464,
-  "cw": 4000,
-  "cfm": 800,
-  "h1": 1800,
-  "h2": 2000,
-  "h3": 200,
-  "h3r": 200,
-  "rhol": 1198,
-  "rhod": 7600,
-  "rhoc": 7800,
-  "rhor": 7800,
-  "rhofm": 2245,
-  "rhow": 1029,
-  "rhocem": 2700,
-  "gt": 0.0238,
-  "wtg": -0.005,
-  "rpm": 100,
-  "t": 2,
-  "tbit": 1.35,
-  "wob": 22.41,
-  "rop": 14.4,
-  "an": 2,
-  "mdt": 3000
-}
+
+def temp_dict(deltaz):
+    dict = {"tin": 20, "ts": 15, "wd": 0, "csgco": 0, "csgso": 0, "csgio": 0, "ddi": 0.101, "ddo": 0.114, "dcsg": 0.216,
+            "dcsgo": 0.24, "dsr": 0.26, "dsro": 0.6, "dri": 0.45, "dro": 0.5334, "dfm": 2, "dcsgci": 0.63,
+            "dcsgco": 0.66, "dcsgsi": 0.41, "dcsgso": 0.44, "tcsr3": 0, "tcem4": 0, "q": 47.696, "lambdal": 0.635,
+            "lambdac": 43.3, "lambdacem": 0.7, "lambdad": 40, "lambdafm": 2.249, "lambdar": 15.49, "lambdarw": 5,
+            "lambdaw": 0.6, "cl": 3713, "cc": 469, "ccem": 2000, "cd": 400, "cr": 464, "cw": 4000, "cfm": 800,
+            "h1": 1800, "h2": 2000, "h3": 200, "h3r": 200, "rhol": 1198, "rhod": 7600, "rhoc": 7800, "rhor": 7800,
+            "rhofm": 2245, "rhow": 1029, "rhocem": 2700, "gt": 0.0238, "wtg": -0.005, "rpm": 100, "t": 2, "tbit": 1.35,
+            "wob": 22.41, "rop": 14.4, "an": 2, "mdt": 3000, "deltaz": deltaz}
+
+    return dict
+
 
 class WellTemperature(object):
     def __init__(self, temp_dict):
         self.tin = temp_dict["tin"]  # Inlet Fluid temperature, °C
         self.ts = temp_dict["ts"]  # Surface Temperature (RKB), °C
         self.wd = temp_dict["wd"]  # Water Depth, m
-        deltaz = 50  # deltaz is always the same  # Length of each grid cell
+        deltaz = temp_dict["deltaz"]
         self.riser = round(self.wd / deltaz)  # number of grid cells for the riser
         self.csgc = round(temp_dict["csgco"] / deltaz)  # Shoe Depth of Conductor Casing, m
         self.csgs = round(temp_dict["csgso"] / deltaz)  # Shoe Depth of Surface Casing, m
