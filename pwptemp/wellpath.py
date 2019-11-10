028631cf463b684fd089fc575c70db57f22921bb
JUANG <camiloang94@gmail.com>
2019-11-10 21:37:17 +0100

    Including J-type well in the function wellpath.get

--- pwptemp/wellpath.py
+++ pwptemp/wellpath.py
@@ -1,11 +1,28 @@
 from numpy import interp, arange
+from math import radians, sin, cos
 
 
-def get(mdt, deltaz):
-    # WELLPROFILE for vertical well
-    md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
-    tvd = md   # True Vertical Depth from RKB, m
-    zstep = len(md)  # Number of cells from RKB up to the bottom
+def get(mdt, deltaz, kop=0, eob=0, build_angle=0, profile='vertical'):
+    if profile == 'vertical':        # WELLPROFILE for vertical well
+        md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
+        tvd = md   # True Vertical Depth from RKB, m
+        zstep = len(md)  # Number of cells from RKB up to the bottom
+
+    if profile == 'J':        # J-type well
+        md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
+        tvd = md[:round(kop / deltaz) + 1]  # True Vertical Depth from RKB, m
+        s = eob - kop
+        theta = radians(build_angle)
+        r = s / theta
+        z_displacement = (r * sin(theta)) / round((eob - kop) / deltaz)
+        # Build section
+        for x in range(round(kop / deltaz), round(eob / deltaz)):
+            tvd.append(tvd[x] + z_displacement)
+        z_displacement = (deltaz * cos(theta))
+        # Hold
+        for x in range(round(eob / deltaz) + 1, len(md)):
+            tvd.append(tvd[x - 1] + z_displacement)
+        zstep = len(md)
 
     class WellDepths(object):
         def __init__(self):
