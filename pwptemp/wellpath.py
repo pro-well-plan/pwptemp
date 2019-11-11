a624ac570909f4bd0c2a6247937b3f807b1cbdba
JUANG <camiloang94@gmail.com>
2019-11-11 04:11:22 +0100

    Including Horizontal single-curve and Horizontal double-curve wells in the function wellpath.get

--- pwptemp/wellpath.py
+++ pwptemp/wellpath.py
@@ -2,60 +2,146 @@ from numpy import interp, arange
 from math import radians, sin, cos
 
 
-def get(mdt, deltaz, kop=0, eob=0, build_angle=0, profile='vertical', sod=0, eod=0):
-    if profile == 'vertical':        # WELLPROFILE for vertical well
+def get(mdt, deltaz, profile='V', build_angle=1, kop=0, eob=0, sod=0, eod=0, kop2=0, eob2=0):
+    if profile == 'V':        # Vertical well
         md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
         tvd = md   # True Vertical Depth from RKB, m
+
         zstep = len(md)  # Number of cells from RKB up to the bottom
 
     if profile == 'J':        # J-type well
         # Vertical section
         md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
         tvd = md[:round(kop / deltaz) + 1]  # True Vertical Depth from RKB, m
-        s = eob - kop
-        theta = radians(build_angle)
-        r = s / theta
-        z_displacement = (r * sin(theta)) / round((eob - kop) / deltaz)
 
         # Build section
-        for x in range(round(kop / deltaz), round(eob / deltaz)):
-            tvd.append(tvd[x] + z_displacement)
-        z_displacement = (deltaz * cos(theta))
+        s = deltaz
+        theta_delta = radians(build_angle / round((eob - kop) / deltaz))
+        theta = theta_delta
+        r = s / theta
+        z_displacement = (r * sin(theta))
+        tvd.append(tvd[-1] + z_displacement)
+        z_count = z_displacement
+        for x in range(round((eob - kop) / deltaz)-1):
+            theta = theta+theta_delta
+            z_displacement = (r * sin(theta)) - z_count
+            tvd.append(tvd[-1] + z_displacement)
+            z_count += z_displacement
 
         # Tangent section
-        for x in range(round(eob / deltaz) + 1, len(md)):
-            tvd.append(tvd[x-1] + z_displacement)
-        zstep = len(md)
+        z_displacement = (deltaz * cos(radians(build_angle)))
+        for x in range(round((mdt-eob)/deltaz)):
+            tvd.append(tvd[-1] + z_displacement)
+
+        zstep = len(md)  # Number of cells from RKB up to the bottom
 
     if profile == 'S':  # S-type well
         # Vertical section
         md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
         tvd = md[:round(kop / deltaz) + 1]  # True Vertical Depth from RKB, m
+
         # Build section
-        s = eob - kop
-        theta = radians(build_angle)
+        s = deltaz
+        theta_delta = radians(build_angle) / round((eob - kop) / deltaz)
+        theta = theta_delta
         r = s / theta
-        z_displacement = (r * sin(theta)) / round((eob - kop) / deltaz)
-        for x in range(round(kop / deltaz), round(eob / deltaz)):
-            tvd.append(tvd[x] + z_displacement)
-        z_displacement = (deltaz * cos(theta))
+        z_displacement = (r * sin(theta))
+        tvd.append(tvd[-1] + z_displacement)
+        z_count = z_displacement
+        for x in range(round((eob - kop) / deltaz) - 1):
+            theta = theta + theta_delta
+            z_displacement = (r * sin(theta)) - z_count
+            tvd.append(tvd[-1] + z_displacement)
+            z_count += z_displacement
 
         # Tangent section
-        for x in range(round(eob / deltaz)+1, round(sod / deltaz)):
-            tvd.append(tvd[x-1] + z_displacement)
-        zstep = len(md)
+        for x in range(round((sod - eob) / deltaz)):
+            tvd.append(tvd[-1] + z_displacement)
 
         # Drop section
-        s = eod - sod
+        s = deltaz
+        theta_delta = radians(build_angle) / round((eod - sod) / deltaz)
         theta = radians(build_angle)
-        r = s / theta
-        z_displacement = (r * sin(theta)) / round((eod - sod) / deltaz)
-        for x in range(round(sod / deltaz), round(eod / deltaz)):
-            tvd.append(tvd[x-1] + z_displacement)
+        r = s / theta_delta
+        for x in range(round((eod - sod) / deltaz)):
+            z_displacement = (r * sin(theta-(theta_delta*x))) - (r * sin(theta-(theta_delta*(x+1))))
+            tvd.append(tvd[-1] + z_displacement)
 
         # Vertical section
-        for x in range(round(eod / deltaz), len(md)):
-            tvd.append(tvd[x-1] + deltaz)
+        for x in range(round((mdt - eod) / deltaz)):
+            tvd.append(tvd[-1] + deltaz)
+
+        zstep = len(md)  # Number of cells from RKB up to the bottom
+
+    if profile == 'H1':        # Horizontal single-curve well
+        # Vertical section
+        md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
+        tvd = md[:round(kop / deltaz) + 1]  # True Vertical Depth from RKB, m
+
+        # Build section
+        s = deltaz
+        theta_delta = radians(90) / round((eob - kop) / deltaz)
+        theta = theta_delta
+        r = s / theta
+        z_displacement = (r * sin(theta))
+        tvd.append(tvd[-1] + z_displacement)
+        z_count = z_displacement
+        for x in range(round((eob - kop) / deltaz)-1):
+            theta = theta+theta_delta
+            z_displacement = (r * sin(theta)) - z_count
+            tvd.append(tvd[-1] + z_displacement)
+            z_count += z_displacement
+
+        # Horizontal section
+        for x in range(round((mdt-eob)/deltaz)):
+            tvd.append(tvd[-1])
+
+        zstep = len(md)  # Number of cells from RKB up to the bottom
+
+    if profile == 'H2':        # Horizontal double-curve well
+        # Vertical section
+        md = list(arange(0, mdt + deltaz, deltaz))  # Measured Depth from RKB, m
+        tvd = md[:round(kop / deltaz) + 1]  # True Vertical Depth from RKB, m
+
+        # Build section
+        s = deltaz
+        theta_delta = radians(build_angle / round((eob - kop) / deltaz))
+        theta = theta_delta
+        r = s / theta
+        z_displacement = (r * sin(theta))
+        tvd.append(tvd[-1] + z_displacement)
+        z_count = z_displacement
+        for x in range(round((eob - kop) / deltaz)-1):
+            theta = theta+theta_delta
+            z_displacement = (r * sin(theta)) - z_count
+            tvd.append(tvd[-1] + z_displacement)
+            z_count += z_displacement
+
+        # Tangent section
+        z_displacement = (deltaz * cos(radians(build_angle)))
+        for x in range(round((kop2-eob)/deltaz)):
+            tvd.append(tvd[-1] + z_displacement)
+
+        # Build section
+        s = deltaz
+        build_angle = 90 - build_angle
+        theta_delta = radians(build_angle / round((eob2 - kop2) / deltaz))
+        theta = theta_delta
+        r = s / theta
+        z_displacement = (r * sin(theta))
+        tvd.append(tvd[-1] + z_displacement)
+        z_count = z_displacement
+        for x in range(round((eob2 - kop2) / deltaz) - 1):
+            theta = theta + theta_delta
+            z_displacement = (r * sin(theta)) - z_count
+            tvd.append(tvd[-1] + z_displacement)
+            z_count += z_displacement
+
+        # Horizontal section
+        for x in range(round((mdt - eob2) / deltaz)):
+            tvd.append(tvd[-1])
+
+        zstep = len(md)  # Number of cells from RKB up to the bottom
 
     class WellDepths(object):
         def __init__(self):
