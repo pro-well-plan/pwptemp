728f467f8219b1be0596c3dc6d253846534c361f
jcamiloangarita <camiloang94@gmail.com>
2020-10-08 13:19:11 +0200

    heat coefficients at drill bit

--- pwptemp/heat_coefficients.py
+++ pwptemp/heat_coefficients.py
@@ -29,12 +29,24 @@ def add_heat_coefficients(well, delta_time):
         y['comp_W'] = (y['tc'] / (well.sr_or * (well.sr_or - well.sr_ir) * log(well.fm_rad / well.sr_or))) / 2
         y['comp_time'] = y['rho'] * y['shc'] / delta_time
 
+    # heat coefficients at drill bit
+    joule = 4.1868  # Joule's constant  [Nm/cal]
+    bit_cell = well.sections[0][-1]
+    q_bit = (1 / joule) * (1 - well.bit_n) * (well.wob * (well.rop / 3600) + 2 * pi * (well.rpm / 60) * well.tbit) \
+        + 0.7 * (well.q / 3600) * (bit_cell['rho'] / (2 * 9.81)) * ((well.q / 3600) / (0.95 * well.an)) ** 2
+    v_bit = well.q / well.an
+    bit_cell['comp_N/S'] = ((bit_cell['rho'] * bit_cell['shc'] * v_bit) / well.depth_step) / 2
+    bit_cell['comp_E'] = (2 * well.h1[-1] / well.annular_or) / 2  # East component
+    bit_cell['comp_HeatSource'] = q_bit / well.an  # Heat source term
+    bit_cell['comp_time'] = bit_cell['rho'] * bit_cell['shc'] / delta_time  # Time component
+
 
 def calc_heat_source(well, torque, f, rho_fluid, case='pipe'):
     if case == 'pipe':
         qp = 2 * pi * (well.rpm / 60) * torque
         + 0.2 * well.q * 2 * f * rho_fluid * (well.vp ** 2) * (well.md[-1] / (well.pipe_id * 127.094 * 10 ** 6))
         heat_source_term = qp / (pi * (well.r1 ** 2))
+
     else:
         qa = (0.085 * (2 * well.k * well.md[-1] / ((well.annular_or - well.r2) * (127.094 * 10 ** 6))) *
               ((2 * (well.n + 1) * well.q) / (well.n * pi * (well.annular_or + well.r2) *
